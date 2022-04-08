905edab83c79c993c802b1e84a43528aa9dea5eb
zachDesktop <92641833+honeyboyJones@users.noreply.github.com>
2022-04-08 14:48:47 -0400

    added event for playercontroller

--- four_person_tactical/Assets/Scripts/BattleManager.cs
+++ four_person_tactical/Assets/Scripts/BattleManager.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour {
 
+    PlayerController playerController;
 
     List<Battler> playerBattlers = new List<Battler>();
     List<Battler> enemyBattlers = new List<Battler>();
+
+    public enum BattleState
+    {
+        BattleStart,
+        PlayerTurn,
+        EnemyTurn,
+        BattleOver
+    }
+
+    private void Start() {
+        if(PlayerController.instance != null)
+        {
+            playerController = PlayerController.instance;
+        }
+        
+    }
 
 
     void TrackTurns() {
--- four_person_tactical/Assets/Scripts/PlayerController.cs
+++ four_person_tactical/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    bool takingTurn;
+    public delegate void OnPlayerAction();
+    public event OnPlayerAction PlayerTurnOverCallback; //anything can subscribe to this event as long as it has a reference to this instance
+
+    #region Singleton
+
+    public static PlayerController instance;
+    private void Awake() {
+        if (instance != null) {
+            Debug.LogWarning("Warning! More than one instance of PlayerController found!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public bool takingTurn;
 
     List<Battler> playerBattlers = new List<Battler>();
 
